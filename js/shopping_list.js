482870cd55ea869497f3f6119ae47a18c582466e
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-20 14:04:40 +0200

    feat : added click function for add button

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,4 +1,14 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const ul = document.getElementById("ul");
-const li = document.getElementById("li");
+
+btn.addEventListener("click", function() {
+    const value = input.value;
+    if (value === ""){
+    return;
+}
+
+const li = document.createElement("li");
+li.textContent = value;
+ul.appendChild(li);
+});
